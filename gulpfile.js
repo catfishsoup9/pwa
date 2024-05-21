b3eb829e1713d78696d7f56f01aec5b55a821e21
m.kokorina <m.kokorina@globus-it.com>
2024-05-21 13:56:20 +0400

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -18,11 +18,11 @@ gulp.task('watch', () => {
   gulp.watch('app/pug/**/*.pug', gulp.series('pug-compile'));
   gulp.watch('app/scss/**/*.scss', gulp.series('scss-compile'));
 });
+//
+// gulp.task('copy-html', () => {
+//   return gulp.src('app/html/hello.html')
+//     .pipe(gulp.dest('dist'));
+// });
 
-gulp.task('copy-html', () => {
-  return gulp.src('app/html/hello.html')
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('build', gulp.series(gulp.parallel('pug-compile', 'scss-compile', 'copy-html')));
+// gulp.task('build', gulp.series(gulp.parallel('pug-compile', 'scss-compile', 'copy-html')));
 gulp.task('default', gulp.series(gulp.parallel('pug-compile', 'scss-compile'), 'watch'));
